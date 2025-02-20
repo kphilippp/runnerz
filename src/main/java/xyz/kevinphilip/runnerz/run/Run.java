f08362ed56402fad987125d64d0b75feb943d955
kphilippp <kevinphilip2004@gmail.com>
2025-02-20 10:13:33 -0600

    added Run validation constraints

--- src/main/java/xyz/kevinphilip/runnerz/run/Run.java
+++ src/main/java/xyz/kevinphilip/runnerz/run/Run.java
@@ -1,15 +1,30 @@
 package xyz.kevinphilip.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 
 // A record also has getters and equals and hashcode function; however it is immutable -> no setters
 public record Run(
     Integer id,
+
+    @NotEmpty
     String title,
     LocalDateTime startedOn,
     LocalDateTime completedOn,
+
+    @Positive
     Integer miles,
     Location location
 ) {
+
+    public Run {
+        if (completedOn.isBefore(startedOn)) {
+            throw new IllegalArgumentException("Completed on must be after started on");
+        }
+    }
+
+
 }
